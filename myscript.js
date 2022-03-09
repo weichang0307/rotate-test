1abf2b71cb6538e2bed73adf1895f5661152c240
weichang0307 <83111522+weichang0307@users.noreply.github.com>
2022-03-09 19:05:28 +0800

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,9 @@
+let text=document.getElementById('text')
+text.innerHTML='hello'
+window.addEventListener('deviceorientation', function(event) {
+    var alpha = event.alpha;
+    var beta = event.beta;
+    var gamma = event.gamma;
+    console.log(event)
+    text.innerHTML=alpha+' '+beta+' '+gamma
+}, false);
