e0803b3f2e685a19c602130ed1119f2352bf8d19
weichang0307 <83111522+weichang0307@users.noreply.github.com>
2022-03-09 19:38:26 +0800

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,9 +1,29 @@
 let text=document.getElementById('text')
 text.innerHTML='hello'
+let alpha
+let beta
+let gamma
+let canvas=document.getElementById('canvas')
+canvas.height=600
+canvas.width=600
+let ctx=canvas.getContext('2d')
+
 window.addEventListener('deviceorientation', function(event) {
-    var alpha = event.alpha;
-    var beta = event.beta;
-    var gamma = event.gamma;
+    alpha = event.alpha
+    beta = event.beta
+    gamma = event.gamma
     console.log(event)
-    text.innerHTML=alpha+'<br>'+beta+'<br>'+gamma
+    text.innerHTML=Math.floor(alpha)+'<br>'+Math.floor(beta)+'<br>'+Math.floor(gamma)
+    draw()
 }, false);
+function draw(){
+    ctx.fillStyle='black'
+    ctx.fillRect(0,0,600,600)
+    ctx.translate(300,300)
+    ctx.rotate(alpha*Math.PI/180)
+    ctx.translate(-15,-15)
+    ctx.fillStyle='red'
+
+    ctx.fillRect(0,0,30,30)
+}
+//setInterval(draw,20)
