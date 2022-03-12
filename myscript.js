a8d94ac392614b8f8508193ffecb96576ac0a393
weichang0307 <83111522+weichang0307@users.noreply.github.com>
2022-03-12 15:05:15 +0800

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -7,7 +7,7 @@ let canvas=document.getElementById('canvas')
 canvas.height=600
 canvas.width=600
 let ctx=canvas.getContext('2d')
-
+/*
 window.addEventListener('deviceorientation', function(event) {
     alpha = event.alpha
     beta = event.beta
@@ -16,12 +16,31 @@ window.addEventListener('deviceorientation', function(event) {
     text.innerHTML=Math.floor(alpha)+'<br>'+Math.floor(beta)+'<br>'+Math.floor(gamma)
     draw()
 }, false);
+*/
+let deg=0
+window.addEventListener('devicemotion', function(event) {
+    alpha = event.rotationRate.alpha
+    beta = event.rotationRate.beta
+    gamma = event.rotationRate.gamma
+    deg+=alpha/100
+    console.log(event.rotationRate)
+    text.innerHTML=Math.floor(alpha)+'<br>'+Math.floor(beta)+'<br>'+Math.floor(gamma)
+    draw()
+}, false);
+
+
+
+
 function draw(){
+    
+
+
+
     ctx.save()
         ctx.fillStyle='black'
         ctx.fillRect(0,0,600,600)
         ctx.translate(300,300)
-        ctx.rotate(-alpha*Math.PI/10)
+        ctx.rotate(deg)
         ctx.fillStyle='red'
         ctx.beginPath()
         ctx.moveTo(-10,-20)
@@ -31,4 +50,4 @@ function draw(){
         ctx.fill()
     ctx.restore()
 }
-//setInterval(draw,20)
+setInterval(draw,20)
